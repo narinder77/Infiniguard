2ac2608825ccc155ac2ebf32c8e4bcb993247923
kamaltech123 <kamal@technoarray.com>
2024-04-22 18:44:23 +0530

    register equipment certification id sorting done

--- app/Http/Controllers/Admin/CertifiedApplicatorController.php
+++ app/Http/Controllers/Admin/CertifiedApplicatorController.php
@@ -17,73 +17,18 @@ class CertifiedApplicatorController extends Controller
         $page_title = 'Certified Applicators';
         $page_description = 'Some description for the page';
 
-        /*if ($request->ajax()) {
+        if ($request->ajax()) {
             $data = CertifiedApplicator::with(['certifiedProviders'])
                 ->withCount('registeredCodes', 'warrantyClaims');
-
             return DataTables::of($data)
                 ->orderColumn('provider_name', function ($query, $order) {
-                    $query->with(['certifiedProviders' => function ($query) use ($order) {
-                        $query->orderBy('certified_providers.provider_name', $order);
-                    }]);  
-                })                  
-                ->filterColumn('provider_name', function ($query, $keyword) {
+                    $query->join('certified_providers', 'certified_applicators.applicator_provider_id', '=', 'certified_providers.provider_id')
+                        ->orderBy('certified_providers.provider_name', $order);
+                })->filterColumn('provider_name', function ($query, $keyword) {
                     $query->whereHas('certifiedProviders', function ($query) use ($keyword) {
                         $query->where('provider_name', 'like', "%{$keyword}%");
                     });
-                })
-                ->toJson();
-        }*/
-        if ($request->ajax()) {
-            $draw = $request->get('draw');
-            $start = $request->get('start');
-            $length = $request->get('length');
-            $order = $request->get('order');
-            $columns = $request->get('columns');
-            $search = $request->get('search')['value'];
-
-            $query = CertifiedApplicator::query();
-            $query->with('certifiedProviders')->withCount('registeredCodes', 'warrantyClaims');
-
-            if (!empty($search)) {
-                $query->when($search, function ($q) use ($search) {
-                    $q->where(function ($q) use ($search) {
-                        $q->where('applicator_id', 'like', "%$search%")
-                            ->orWhere('applicator_certification_id', 'like', "%$search%")
-                            ->orWhere('applicator_name', 'like', "%$search%")
-                            ->orWhere('applicator_email', 'like', "%$search%")
-                            ->orWhere('applicator_date', 'like', "%$search%");
-                    })->orWhereHas('certifiedProviders', function ($q) use ($search) {
-                        $q->where('provider_name', 'like', "%$search%");
-                    });
-                });
-            }    
-            if (!empty($order)) {
-                $columnIndex = $order[0]['column'];
-                $columnName = $columns[$columnIndex]['data'];
-                $columnSortOrder = (isset($order[0]['dir']) && $order[0]['dir'] === 'desc') ? 'desc' : 'asc';
-                if (strpos($columnName, '.') !== false) {
-                  [$relationship, $relatedColumnName] = explode('.', $columnName);
-                  $query->join($relationship, function ($join) use ($relatedColumnName, $columnSortOrder) {
-                    $join->orderBy($relatedColumnName, $columnSortOrder);
-                  });
-                } else {
-                  $query->orderBy($columnName, $columnSortOrder);
-                }
-              }
-            // Pagination
-            $total = $query->count();
-            $filteredTotal = $query->count();
-            $query->skip($start)->take($length);
-            $data = $query->get();
-
-            $response = [
-                "draw" => intval($draw),
-                "recordsTotal" => $total,
-                "recordsFiltered" => $filteredTotal,
-                "data" => $data,
-            ];
-            return $response;
+                })->toJson();
         }
         return view('admin.certified-applicators.index', compact('page_title', 'page_description'));
     }
